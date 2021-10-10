fe0ea0ed1968ea9d088eba3c5d3b384253ee694a
Matrix53 <1079207272@qq.com>
2021-10-10 13:14:33 +0800

    feat: add std-5 des-5

--- BUAAOJ-X/Matrix53-5/standard.cpp
+++ BUAAOJ-X/Matrix53-5/standard.cpp
@@ -6,8 +6,9 @@ typedef long long ll;
 typedef pair<ll, ll> pll;
 
 const ll mod = 998244353;
+const int maxn = 1e5 + 5;
 
-pll data[100005];
+pll data[maxn];
 
 bool isLower(pll a, pll b) {
   return a.first * b.second + a.second < b.first * a.second + b.second;
@@ -19,7 +20,9 @@ int main() {
   for (int i = 0; i < n; ++i) scanf("%lld", &data[i].first);
   for (int i = 0; i < n; ++i) scanf("%lld", &data[i].second);
   sort(data, data + n, isLower);
+  ll ans = 1;
   for (int i = 0; i < n; ++i)
-    printf("%lld %lld\n", data[i].first, data[i].second);
+    ans = (ans * data[i].first + data[i].second) % mod;
+  printf("%lld", ans);
   return 0;
 }
