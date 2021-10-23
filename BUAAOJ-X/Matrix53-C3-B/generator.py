211edc2f366d75c28db07273c1dbedbd193406d6
Matrix53 <1079207272@qq.com>
2021-10-24 00:32:35 +0800

    feat: add two gen

--- BUAAOJ-X/Matrix53-C3-B/generator.py
+++ BUAAOJ-X/Matrix53-C3-B/generator.py
@@ -1,22 +1,19 @@
 from cyaron import *
 
-code_path = 'D:/Workspace/algo/BUAAOJ-X/Matrix53-5/'
+code_path = 'D:/Workspace/algo/BUAAOJ-X/Matrix53-C3-B/'
 
-# # small data
-# for i in range(5):
-#     io = IO(str(i + 1) + '.in', str(i + 1) + '.out')
-#     io.input_writeln(10)
-#     seq = Sequence(lambda i, f: randint(1, 100))
-#     io.input_writeln(seq.get(0, 10))
-#     io.input_writeln(seq.get(11, 21))
-#     Compare.program(code_path + 'standard.exe',
-#                     input=io,
-#                     std_program=code_path + 'compare.exe')
-#     io.close()
+# small data
+io = IO('1.in', '1.out')
+io.input_writeln(10)
+seq = Sequence(lambda i, f: randint(1, 100))
+io.input_writeln(seq.get(0, 10))
+io.input_writeln(seq.get(11, 21))
+io.output_gen(code_path + 'standard.exe')
+io.close()
 
 # big data
-for i in range(5):
-    io = IO(str(i + 1) + '.in', str(i + 1) + '.out')
+for i in range(3):
+    io = IO(str(i + 2) + '.in', str(i + 2) + '.out')
     io.input_writeln(100000)
     seq = Sequence(lambda i, f: randint(1, 10000000))
     io.input_writeln(seq.get(0, 100000))
@@ -24,10 +21,10 @@ for i in range(5):
     io.output_gen(code_path + 'standard.exe')
     io.close()
 
-# # hack data
-# io = IO('5.in', '5.out')
-# io.input_writeln(3500)
-# seq = Sequence(lambda i, f: 9999999)
-# io.input_writeln(seq.get(0, 3499))
-# io.output_gen(code_path + 'standard.exe')
-# io.close()
+# hack data
+io = IO('5.in', '5.out')
+io.input_writeln(3500)
+seq = Sequence(lambda i, f: 9999999)
+io.input_writeln(seq.get(0, 3499))
+io.output_gen(code_path + 'standard.exe')
+io.close()
