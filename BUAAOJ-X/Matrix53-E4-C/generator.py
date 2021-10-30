f977e193e1e9c8e19279c1e3b15a32d3fb6c39da
Matrix53 <1079207272@qq.com>
2021-10-30 10:32:17 +0800

    chore:

--- BUAAOJ-X/Matrix53-E4-C/generator.py
+++ BUAAOJ-X/Matrix53-E4-C/generator.py
@@ -8,6 +8,9 @@ io.input_writeln(500)
 seq = Sequence(lambda i, f: randint(1, 10000))
 io.input_writeln(seq.get(1, 500))
 io.input_writeln(seq.get(501, 1001))
+# Compare.program(code_path + 'standard.exe',
+#                 input=io,
+#                 std_program=code_path + 'compare.exe')
 io.output_gen(code_path + 'standard.exe')
 io.close()
 
