ab427ce71169527b9447074417e1d0d30c5ea3c8
Matrix53 <1079207272@qq.com>
2021-10-30 17:14:48 +0800

    feat: C4-F

--- BUAAOJ-X/Matrix53-C4-F/standard.cpp
+++ BUAAOJ-X/Matrix53-C4-F/standard.cpp
@@ -1,28 +1,23 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-const int MAX_SUM = 250005;
+#include <cstdio>
+#define MAX_SUM 250005
 
 bool dp[MAX_SUM];
 
 int main() {
-  // freopen("D:/Workspace/algo/BUAAOJ-X/Matrix53-C4-F/data/2.in", "r", stdin);
-  int n, x, sum = 0;
+  int n, x, i, sum = 0;
   dp[0] = true;
   scanf("%d", &n);
   while (n--) {
     scanf("%d", &x);
     sum += x;
-    for (int i = MAX_SUM; i >= x; --i) {
+    for (i = MAX_SUM; i >= x; --i) {
       if (dp[i - x]) dp[i] = true;
     }
   }
-  for (int i = sum / 2; i >= 0; --i) {
+  for (i = sum / 2; i >= 0; --i) {
     if (dp[i]) {
-      x = i;
-      break;
+      printf("%d", sum - i * 2);
+      return 0;
     }
   }
-  printf("%d", sum - x * 2);
-  return 0;
 }
