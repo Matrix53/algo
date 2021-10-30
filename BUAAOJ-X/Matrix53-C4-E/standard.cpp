0fc0b94b35fb3fdcd67bde0171cab5ae1eb04ac7
Matrix53 <1079207272@qq.com>
2021-10-31 00:11:33 +0800

    feat: C4-E

---
+++ BUAAOJ-X/Matrix53-C4-E/standard.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e5 + 5;
+pair<int, int> b[maxn];
+int ans[maxn];
+int main() {
+  int n;
+  scanf("%d", &n);
+  for (int i = 0; i < n; ++i) {
+    scanf("%d", &b[i].first);
+    b[i].second = i;
+  }
+  sort(b, b + n);
+  for (int i = 0; i < n; ++i) ans[b[i].second] = b[n - i - 1].first;
+  for (int i = 0; i < n; ++i) printf("%d ", ans[i]);
+  return 0;
+}
