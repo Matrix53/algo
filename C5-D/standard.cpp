e23bce8cf338a6595e3e8b6f53166b4a9c702602
Matrix53 <1079207272@qq.com>
2021-11-05 00:05:36 +0800

    feat: C5-D and E5-D

---
+++ C5-D/standard.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const int N = 1e4 + 5, M = 2e5 + 5;
+const ll inf = LLONG_MAX;
+
+int tot = 1, lnk[N], ter[M], nxt[M], dep[N], cur[N];
+ll val[M];
+
+void add(int u, int v, ll w) {
+  ter[++tot] = v;
+  nxt[tot] = lnk[u];
+  lnk[u] = tot;
+  val[tot] = w;
+}
+
+void addedge(int u, int v, ll w) {
+  add(u, v, w);
+  add(v, u, 0);
+}
+
+int bfs(int s, int t) {
+  memset(dep, 0, sizeof(dep));
+  memcpy(cur, lnk, sizeof(lnk));
+  queue<int> q;
+  q.push(s);
+  dep[s] = 1;
+  while (!q.empty()) {
+    int u = q.front();
+    q.pop();
+    for (int i = lnk[u]; i; i = nxt[i]) {
+      int v = ter[i];
+      if (val[i] && !dep[v]) {
+        q.push(v);
+        dep[v] = dep[u] + 1;
+      }
+    }
+  }
+  return dep[t];
+}
+
+ll dfs(int u, int t, ll flow) {
+  if (u == t) return flow;
+  ll ans = 0;
+  for (int &i = cur[u]; i && ans < flow; i = nxt[i]) {
+    int v = ter[i];
+    if (val[i] && dep[v] == dep[u] + 1) {
+      ll x = dfs(v, t, min(val[i], flow - ans));
+      if (x) {
+        val[i] -= x;
+        val[i ^ 1] += x;
+        ans += x;
+      }
+    }
+  }
+  if (ans < flow) dep[u] = -1;
+  return ans;
+}
+
+ll dinic(int s, int t) {
+  ll ans = 0;
+  while (bfs(s, t)) {
+    ll x;
+    while ((x = dfs(s, t, inf))) ans += x;
+  }
+  return ans;
+}
+
+int main() {
+  int n, m, p, t;
+  scanf("%d%d%d%d", &n, &m, &p, &t);
+  for (int i = 0; i < m; ++i) {
+    int u, v, c;
+    scanf("%d%d%d", &u, &v, &c);
+    addedge(u, v, c);
+  }
+  for (int i = 0; i < p; ++i) {
+    int q;
+    scanf("%d", &q);
+    addedge(n + 1, q, inf);
+  }
+  printf("%lld", dinic(n + 1, t));
+  return 0;
+}
---
+++ E5-D/standard.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const ll inf = LLONG_MAX / 2;
+const int maxv = 1e5 + 5;
+
+struct edge {
+  int to;
+  ll w;
+  edge(int a, ll b) : to(a), w(b) {}
+};
+struct qnode {
+  int id;
+  ll ndis;
+  qnode(int a, ll b) : id(a), ndis(b) {}
+  bool operator<(const qnode& a) const { return ndis > a.ndis; }
+};
+
+vector<edge> g[maxv];
+int n, m;
+ll dis[maxv];
+bool done[maxv];
+
+void dijkstra(int s) {
+  for (int i = 1; i <= n; ++i) {
+    dis[i] = inf;
+    done[i] = false;
+  }
+  dis[s] = 0;
+  priority_queue<qnode> q;
+  q.push(qnode(s, 0));
+  while (!q.empty()) {
+    qnode u = q.top();
+    q.pop();
+    if (done[u.id]) continue;
+    done[u.id] = true;
+    for (auto v : g[u.id]) {
+      if (done[v.to]) continue;
+      if (dis[v.to] > u.ndis + v.w) {
+        dis[v.to] = u.ndis + v.w;
+        q.push(qnode(v.to, dis[v.to]));
+      }
+    }
+  }
+}
+
+int main() {
+  int p, x, y;
+  scanf("%d%d", &n, &m);
+  scanf("%d%d%d", &p, &x, &y);
+  for (int i = 0; i < m; ++i) {
+    int u, v, t;
+    scanf("%d%d%d", &u, &v, &t);
+    g[u].push_back(edge(v, t));
+    g[v].push_back(edge(u, t));
+  }
+  dijkstra(p);
+  ll px = dis[x], py = dis[y];
+  dijkstra(x);
+  ll xy = dis[y];
+  if (px == inf || py == inf || xy == inf) {
+    printf("-1");
+  } else {
+    printf("%lld", xy + min(px, py));
+  }
+  return 1;
+}
