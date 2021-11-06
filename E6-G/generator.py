7e3e220c8fcad2c50ddea6e75a458505eca3af01
Matrix53 <1079207272@qq.com>
2021-11-06 19:22:20 +0800

    E6-G

--- E6-G/generator.py
+++ E6-G/generator.py
@@ -1,28 +1,33 @@
 from cyaron import *
 
-code_path = 'D:/Workspace/algo/BUAAOJ-X/Matrix53-C3-A/'
+code_path = 'D:/Workspace/algo/E6-G/'
 
 # small data
 io = IO('1.in', '1.out')
-io.input_writeln(100, 100)
-for i in range(100):
-    io.input_writeln(randint(1, 1000), randint(1, 100))
+io.input_writeln(500)
+seq = Sequence(lambda i, f: randint(1, 10000))
+io.input_writeln(seq.get(1, 500))
+io.input_writeln(seq.get(501, 1001))
+# Compare.program(code_path + 'standard.exe',
+#                 input=io,
+#                 std_program=code_path + 'compare.exe')
 io.output_gen(code_path + 'standard.exe')
 io.close()
 
 # big data
-for i in range(3):
-    io = IO(str(i + 2) + '.in', str(i + 2) + '.out')
-    io.input_writeln(1000, 100000)
-    for j in range(1000):
-        io.input_writeln(randint(1, 10000000), randint(1, 110000))
-    io.output_gen(code_path + 'standard.exe')
-    io.close()
+io = IO('2.in', '2.out')
+io.input_writeln(1000)
+seq = Sequence(lambda i, f: randint(1, 100000000))
+io.input_writeln(seq.get(1, 1000))
+io.input_writeln(seq.get(1001, 2001))
+io.output_gen(code_path + 'standard.exe')
+io.close()
 
 # hack data
-io = IO('5.in', '5.out')
-io.input_writeln(1000, 100000)
-for i in range(1000):
-    io.input_writeln(10000000, 1)
+io = IO('3.in', '3.out')
+io.input_writeln(1000)
+seq = Sequence(lambda i, f: 100000000)
+io.input_writeln(seq.get(1, 1000))
+io.input_writeln(seq.get(1001, 2001))
 io.output_gen(code_path + 'standard.exe')
 io.close()
