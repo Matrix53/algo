07e99333e14a802656c9da41db964946fa18db6a
Matrix53 <1079207272@qq.com>
2021-11-08 15:17:36 +0800

    feat: add E5-B

---
+++ E5-B/standard.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll dp[505][505], g[505][505];
+
+int main() {
+  int n, m, p;
+  scanf("%d%d%d", &n, &m, &p);
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      g[i][j] = LONG_LONG_MAX;
+      dp[i][j] = LONG_LONG_MAX;
+    }
+    g[i][i] = 0;
+    dp[i][i] = 0;
+  }
+  for (int i = 0; i < m; ++i) {
+    int u, v, w;
+    scanf("%d%d%d", &u, &v, &w);
+    g[u][v] = min(g[u][v], ll(w));
+  }
+  for (int k = 1; k <= n; ++k) {
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        if (g[i][k] != LONG_LONG_MAX && g[k][j] != LONG_LONG_MAX &&
+            g[i][k] + g[k][j] < dp[i][j]) {
+          dp[i][j] = g[i][k] + g[k][j];
+        }
+      }
+    }
+  }
+  while (p--) {
+    int u, v;
+    scanf("%d%d", &u, &v);
+    printf("%lld\n", dp[u][v]);
+  }
+  return 0;
+}
