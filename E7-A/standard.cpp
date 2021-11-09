e2b11f69f8f81b2603d04bf3449510ff9bcea3c3
Matrix53 <1079207272@qq.com>
2021-11-09 14:51:32 +0800

    chore:

--- E7-A/standard.cpp
+++ E7-A/standard.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Point3 {
+  double x, y, z;
+  Point3() {}
+  Point3(double a, double b, double c) : x(a), y(b), z(c) {}
+};
+
+typedef Point3 Vector3;
+double dot(Vector3 a, Vector3 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
+Vector3 det(Vector3 a, Vector3 b) {
+  return Point3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z,
+                a.x * b.y - a.y * b.x);
+}
+
 int main() {
-  ;
+  Point3 a, b, c;
+  scanf("%lf%lf%lf%lf%lf%lf%lf%lf%lf", &a.x, &a.y, &a.z, &b.x, &b.y, &b.z, &c.x,
+        &c.y, &c.z);
+  printf("%f", fabs(dot(det(a, b), c)));
   return 0;
 }
