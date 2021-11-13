5661f129979b08afcbee2fd104093924b4f5e65e
Matrix53 <1079207272@qq.com>
2021-11-13 11:07:13 +0800

    fix: E5-B

--- E5-B/generator.py
+++ E5-B/generator.py
@@ -4,8 +4,8 @@ code_path = 'D:/Workspace/algo/E5-B/'
 
 # big data
 io = IO('1.in', '1.out')
-io.input_writeln(500, 25000, 250000)
-graph = Graph.graph(500, 25000, directed=True, weight_limit=(0, 10000000))
+io.input_writeln(500, 1000, 250000)
+graph = Graph.graph(500, 1000, directed=True, weight_limit=(0, 10000000))
 io.input_writeln(graph.to_str(shuffle=True))
 for u in range(1, 501):
     for v in range(1, 501):
--- E5-B/standard.cpp
+++ E5-B/standard.cpp
@@ -2,30 +2,28 @@
 using namespace std;
 typedef long long ll;
 
-ll dp[505][505], g[505][505];
+ll dp[505][505];
 
 int main() {
   int n, m, p;
   scanf("%d%d%d", &n, &m, &p);
   for (int i = 1; i <= n; ++i) {
     for (int j = 1; j <= n; ++j) {
-      g[i][j] = LONG_LONG_MAX;
       dp[i][j] = LONG_LONG_MAX;
     }
-    g[i][i] = 0;
     dp[i][i] = 0;
   }
   for (int i = 0; i < m; ++i) {
     int u, v, w;
     scanf("%d%d%d", &u, &v, &w);
-    g[u][v] = min(g[u][v], ll(w));
+    dp[u][v] = min(dp[u][v], ll(w));
   }
   for (int k = 1; k <= n; ++k) {
     for (int i = 1; i <= n; ++i) {
       for (int j = 1; j <= n; ++j) {
-        if (g[i][k] != LONG_LONG_MAX && g[k][j] != LONG_LONG_MAX &&
-            g[i][k] + g[k][j] < dp[i][j]) {
-          dp[i][j] = g[i][k] + g[k][j];
+        if (dp[i][k] != LONG_LONG_MAX && dp[k][j] != LONG_LONG_MAX &&
+            dp[i][k] + dp[k][j] < dp[i][j]) {
+          dp[i][j] = dp[i][k] + dp[k][j];
         }
       }
     }
